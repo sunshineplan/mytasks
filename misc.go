24f6e23be57af3c1a9660f20b89758268ead19a9
sunshineplan <sunshineplan@users.noreply.github.com>
2021-02-04 20:15:49 +0800

    Default uid is same as username

--- misc.go
+++ misc.go
@@ -15,9 +15,10 @@ import (
 
 func addUser(username string) {
 	log.Print("Start!")
-	if _, err := db.Exec("INSERT INTO user(username) VALUES (?)", strings.ToLower(username)); err != nil {
+	username = strings.TrimSpace(strings.ToLower(username))
+	if _, err := db.Exec("INSERT INTO user(username, uid) VALUES (?, ?)", username, username); err != nil {
 		if strings.Contains(err.Error(), "Duplicate entry") {
-			log.Fatalf("Username %s already exists.", strings.ToLower(username))
+			log.Fatalf("Username %s already exists.", username)
 		} else {
 			log.Fatalln("Failed to add user:", err)
 		}
@@ -27,14 +28,15 @@ func addUser(username string) {
 
 func deleteUser(username string) {
 	log.Print("Start!")
-	res, err := db.Exec("DELETE FROM user WHERE username=?", strings.ToLower(username))
+	username = strings.TrimSpace(strings.ToLower(username))
+	res, err := db.Exec("DELETE FROM user WHERE username = ?", username)
 	if err != nil {
 		log.Fatalln("Failed to delete user:", err)
 	}
 	if n, err := res.RowsAffected(); err != nil {
 		log.Fatalln("Failed to get affected rows:", err)
 	} else if n == 0 {
-		log.Fatalf("User %s does not exist.", strings.ToLower(username))
+		log.Fatalf("User %s does not exist.", username)
 	}
 	log.Print("Done!")
 }
